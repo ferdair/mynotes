1fc0b7ea7b5b891a796768e3a9cec09f38212e8b
Dair Sanchez <dairsanchez@gmail.com>
2022-04-08 18:30:05 -0500

    step-2

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:test/views/login_view.dart';
+import 'package:test/views/register_view.dart';
 import 'firebase_options.dart';
 
 void main() {
@@ -13,6 +14,10 @@ void main() {
         primarySwatch: Colors.blue,
       ),
       home: const HomePage(),
+      routes: {
+        '/login/': (context) => const LoginView(),
+        '/register/': (context) => const RegisterView()
+      },
     ),
   );
 }
@@ -22,29 +27,29 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-      ),
-      body: FutureBuilder(
-        future: Firebase.initializeApp(
-            options: DefaultFirebaseOptions.currentPlatform),
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.done:
-              final user = FirebaseAuth.instance.currentUser;
+    return FutureBuilder(
+      future: Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.done:
+            /*  final user = FirebaseAuth.instance.currentUser;
 
-              if (user?.emailVerified ?? false) {
-                print('YOu are verified user');
-              } else {
-                print('You need to verify your email first');
-              }
+            if (user?.emailVerified ?? false) {
               return const Text('Done');
-            default:
-              return const Text('Loading...');
-          }
-        },
-      ),
+            } else {
+              /*  Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const VerifyEmailView(),
+                  ),
+                ); */
+            } */
+            return const LoginView();
+
+          default:
+            return const CircularProgressIndicator();
+        }
+      },
     );
   }
 }
